b20a58487d611f64932b32a907bb4146af4cbd1b
Ghayoung <wizardlover_@naver.com>
2018-06-11 18:58:02 +0900

    feat: City DTO 내용추가

--- src/main/java/skhu/artview/dto/City.java
+++ src/main/java/skhu/artview/dto/City.java
@@ -2,4 +2,23 @@ package skhu.artview.dto;
 
 public class City {
 
+	int id;
+	String name;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
 }
