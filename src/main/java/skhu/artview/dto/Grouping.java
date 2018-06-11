0ba92e4cc28c6cb8514779a44b6ee5c70ff9d3de
Ghayoung <wizardlover_@naver.com>
2018-06-11 19:04:51 +0900

    fix: Grouping.java

--- src/main/java/skhu/artview/dto/Grouping.java
+++ src/main/java/skhu/artview/dto/Grouping.java
@@ -5,25 +5,39 @@ public class Grouping {
 	int id;
 	int district_id;
 	int artist_id;
+	int group_id;
+
+	public int getGroup_id() {
+		return group_id;
+	}
+
+	public void setGroup_id(int group_id) {
+		this.group_id = group_id;
+	}
+
 	public int getId() {
 		return id;
 	}
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public int getDistrict_id() {
 		return district_id;
 	}
+
 	public void setDistrict_id(int district_id) {
 		this.district_id = district_id;
 	}
+
 	public int getArtist_id() {
 		return artist_id;
 	}
+
 	public void setArtist_id(int artist_id) {
 		this.artist_id = artist_id;
 	}
-	
-	
+
+
 }
 
