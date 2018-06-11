c4e83d7f5fdb376a9cbb45c4c0255d4578467df0
Ghayoung <wizardlover_@naver.com>
2018-06-11 19:11:08 +0900

    feat: Facility DTO 내용추가

--- src/main/java/skhu/artview/dto/Facility.java
+++ src/main/java/skhu/artview/dto/Facility.java
@@ -2,4 +2,23 @@ package skhu.artview.dto;
 
 public class Facility {
 
+	int id;
+	String type;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
 }
