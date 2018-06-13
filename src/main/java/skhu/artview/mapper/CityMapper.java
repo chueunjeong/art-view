9dcefc2a8eb18e1153359df57f134eff2f911d3f
Ghayoung <wizardlover_@naver.com>
2018-06-13 15:32:52 +0900

    feat: Create CityMapper.java

---
+++ src/main/java/skhu/artview/mapper/CityMapper.java
@@ -0,0 +1,11 @@
+package skhu.artview.mapper;
+//import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+//import skhu.artview.dto.City;
+
+@Mapper
+public interface CityMapper {
+
+}
