2ea5e547594910026fefeddfec4e73d7bf250623
Ghayoung <wizardlover_@naver.com>
2018-06-13 15:35:55 +0900

    feat: CityMapper.java 내용추가

--- src/main/java/skhu/artview/mapper/CityMapper.java
+++ src/main/java/skhu/artview/mapper/CityMapper.java
@@ -1,11 +1,18 @@
 package skhu.artview.mapper;
-//import java.util.List;
+import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 
-//import skhu.artview.dto.City;
+import skhu.artview.dto.City;
 
 @Mapper
 public interface CityMapper {
+
+	City findOne(int id);
+	City findOneByName(String name);
+	List<City> findAll();
+	void insert(City city);
+	void update(City city);
+	void delete(int id);
 
 }
