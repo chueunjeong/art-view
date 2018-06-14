46f4623da5b4afb0b590c42ba30ff1c339ed5e3b
hayoung <wizardlover_@naver.com>
2018-06-14 15:33:12 +0900

    feat: Create Fav_artfieldMapper.java

---
+++ src/main/java/skhu/artview/mapper/Fav_artfieldMapper.java
@@ -0,0 +1,19 @@
+package skhu.artview.mapper;
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import skhu.artview.dto.Fav_artfield;
+
+@Mapper
+public interface Fav_artfieldMapper {
+
+	Fav_artfield findOne(int id);
+	List<Fav_artfield> findByUserId(int user_id);
+	Fav_artfield findOneByArtfieldId(int artfield_id);
+	List<Fav_artfield> findAll();
+	void insert(Fav_artfield fav_artfield);
+	void update(Fav_artfield fav_artfield);
+	void delete(int id);
+
+}
