a623aac3b957a6a3e436810e8a99cf99fb2615b4
hayoung <wizardlover_@naver.com>
2018-06-14 15:11:30 +0900

    feat: Create FacilityMapper.java

---
+++ src/main/java/skhu/artview/mapper/FacilityMapper.java
@@ -0,0 +1,18 @@
+package skhu.artview.mapper;
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import skhu.artview.dto.Facility;
+
+@Mapper
+public interface FacilityMapper {
+
+	Facility findOne(int id);
+	Facility findOneByType(String type);
+	List<Facility> findAll();
+	void insert(Facility facility);
+	void update(Facility facility);
+	void delete(int id);
+
+}
