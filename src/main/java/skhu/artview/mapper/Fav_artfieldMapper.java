2a5954177eca6b17ccd79062fdc2202ab883dc9d
hayoung <wizardlover_@naver.com>
2018-06-14 15:46:38 +0900

    fix: Fav_artfieldMapper, Fav_districtMapper 수정

--- src/main/java/skhu/artview/mapper/Fav_artfieldMapper.java
+++ src/main/java/skhu/artview/mapper/Fav_artfieldMapper.java
@@ -10,7 +10,7 @@ public interface Fav_artfieldMapper {
 
 	Fav_artfield findOne(int id);
 	List<Fav_artfield> findByUserId(int user_id);
-	Fav_artfield findOneByArtfieldId(int artfield_id);
+	List<Fav_artfield> findByArtfieldId(int artfield_id);
 	List<Fav_artfield> findAll();
 	void insert(Fav_artfield fav_artfield);
 	void update(Fav_artfield fav_artfield);
--- src/main/java/skhu/artview/mapper/Fav_districtMapper.java
+++ src/main/java/skhu/artview/mapper/Fav_districtMapper.java
@@ -10,7 +10,7 @@ public interface Fav_districtMapper {
 
 	Fav_district findOne(int id);
 	List<Fav_district> findByUserId(int user_id);
-	Fav_district findOneByDistrictId(int district_id);
+	List<Fav_district> findByDistrictId(int district_id);
 	List<Fav_district> findAll();
 	void insert(Fav_district fav_district);
 	void update(Fav_district fav_district);
