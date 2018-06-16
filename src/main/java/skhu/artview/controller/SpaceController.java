40cf5dae9c8d307cbf7d6be63773c97503026468
irissun9602 <iris9602@naver.com>
2018-06-16 22:35:49 +0900

    잠시 주석처리

--- src/main/java/skhu/artview/controller/SpaceController.java
+++ src/main/java/skhu/artview/controller/SpaceController.java
@@ -2,6 +2,7 @@ package skhu.artview.controller;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import skhu.artview.model.Space;
@@ -28,6 +30,18 @@ public class SpaceController {
 			
 	       return spaceService.searchSpace(keyword,10,1);
 	   }
+	
+	
+	/*
+	//카카오페이 연동 결제 서비스
+	@RequestMapping(value="kakaologin", produces= "application/json", 
+			method = {RequestMethod.GET,RequestMethod.POST})
+	public String kakaoLogin(@RequestParam("code") String code, HttpServletRequest request, HttpServletResponse response) {
+		//로그인 후 code 얻음
+		System.out.println("code: " +code);
+		return code;
+	}*/
+	
 
 
 }
