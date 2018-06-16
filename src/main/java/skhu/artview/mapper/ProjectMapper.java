1e7f8da0097547b6cc45ccb47e3c75fac7d838ee
irissun9602 <iris9602@naver.com>
2018-06-16 22:35:38 +0900

    칼럼 수정 submit -> title

--- src/main/java/skhu/artview/mapper/ProjectMapper.java
+++ src/main/java/skhu/artview/mapper/ProjectMapper.java
@@ -8,7 +8,7 @@ import skhu.artview.dto.Project;
 @Mapper
 public interface ProjectMapper {
 	Project findOne(int id);
-	List<Project> findBySubject(String subject);
+	List<Project> findByTitle(String title);
     List<Project> findAll();
     void insert(Project project);
     void delete(int id);
