2f43e18c4f75ac422d5cb95abf682878a9edec4e
irissun9602 <iris9602@naver.com>
2018-06-21 23:17:07 +0900

    boardId추가

--- src/main/java/skhu/artview/dto/QnA.java
+++ src/main/java/skhu/artview/dto/QnA.java
@@ -3,19 +3,27 @@ package skhu.artview.dto;
 public class QnA {
 
 	int id;
+	int boardId;
 	String userId;
 	String title;
 	String content;
 	String type;		//질문, 답변 구분
 	
 	FileDTO file;
-	User user;
+	User username;
 	int hits;
 	public int getId() {
 		return id;
 	}
 	public void setId(int id) {
 		this.id = id;
+	}
+	
+	public int getBoardId() {
+		return boardId;
+	}
+	public void setBoardId(int boardId) {
+		this.boardId = boardId;
 	}
 	public String getUserId() {
 		return userId;
@@ -47,11 +55,12 @@ public class QnA {
 	public void setFile(FileDTO file) {
 		this.file = file;
 	}
-	public User getUser() {
-		return user;
+	
+	public User getUsername() {
+		return username;
 	}
-	public void setUser(User user) {
-		this.user = user;
+	public void setUsername(User username) {
+		this.username = username;
 	}
 	public int getHits() {
 		return hits;
