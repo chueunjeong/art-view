a90eaa18bb10ca61d2742beee118ae9977f31520
hayoung <wizardlover_@naver.com>
2018-06-22 00:35:06 +0900

    gitIgnoreTest

--- src/main/java/skhu/artview/config/AWSClient.java
+++ src/main/java/skhu/artview/config/AWSClient.java
@@ -1,6 +1,7 @@
 package skhu.artview.config;
 
 //AWS 접속을 위한 설정을 해주는 클래스, gitignore 등록 필요, 등록 후 실제 값을 채워넣어야 함
+//gitIgnoreTest
 public class AWSClient {
 
 	final private String accessKey = "accessKey";
--- src/main/java/skhu/artview/service/S3Service.java
+++ src/main/java/skhu/artview/service/S3Service.java
@@ -1,5 +1,6 @@
 package skhu.artview.service;
 
+//gitIgnoreTest
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
