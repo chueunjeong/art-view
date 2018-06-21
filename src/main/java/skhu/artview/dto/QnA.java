d48b332a75b07548b58ee139941fc3b328dc0344
irissun9602 <iris9602@naver.com>
2018-06-21 22:39:39 +0900

    QnA dto 추가 생성

---
+++ src/main/java/skhu/artview/dto/QnA.java
@@ -0,0 +1,65 @@
+package skhu.artview.dto;
+
+public class QnA {
+
+	int id;
+	String userId;
+	String title;
+	String content;
+	String type;		//질문, 답변 구분
+	
+	FileDTO file;
+	User user;
+	int hits;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public FileDTO getFile() {
+		return file;
+	}
+	public void setFile(FileDTO file) {
+		this.file = file;
+	}
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	public int getHits() {
+		return hits;
+	}
+	public void setHits(int hits) {
+		this.hits = hits;
+	}
+	
+	
+}
+
