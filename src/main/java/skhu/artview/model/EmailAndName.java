4978ce6dac56729f5796d628c28ec2e87fe4930b
irissun9602 <iris9602@naver.com>
2018-06-24 18:57:45 +0900

    EmailAndName 모델 객체 생성 : 임시비밀번호 발급용 입력 모델 객체

---
+++ src/main/java/skhu/artview/model/EmailAndName.java
@@ -0,0 +1,21 @@
+package skhu.artview.model;
+
+public class EmailAndName {
+
+	String email;
+	String name;
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
