84ec13cfc3a6961f77d59b45815ba5438f5d4a9a
irissun9602 <iris9602@naver.com>
2018-06-24 19:12:08 +0900

    임시 비밀번호 생성, 데이터베이스 업데이트, 사용자 이메일로 임시 비밀번호 발송 서비스

--- src/main/java/skhu/artview/service/UserService.java
+++ src/main/java/skhu/artview/service/UserService.java
@@ -1,9 +1,13 @@
 package skhu.artview.service;
 import java.util.List;
 
+import javax.mail.MessagingException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import net.skhu.email.utils.Encryption;
+import net.skhu.email.utils.RandomPassword;
 import skhu.artview.model.EmailAndName;
 import skhu.artview.dto.User;
 import skhu.artview.mapper.UserMapper;
@@ -32,10 +36,21 @@ public class UserService {
 			return userMapper.findAll();
 		}
 
-		
+		//임시 비밀번호 발급을 위한 회원정보 조회 (이메일 + 이름)
 		public User findUserByEmailAndName(EmailAndName en){
 			return userMapper.findByEmailAndName(en);
 			
 		}
+		
+		//임시 비밀번호 생성, 데이터베이스 업데이트, 사용자 이메일로 임시 비밀번호 발송
+		public String setNewPassword(User user) throws MessagingException {
+			String newPassword = RandomPassword.getRamdomPassword(4);
+			String setPassword = Encryption.encrypt(newPassword, Encryption.MD5);
+			userMapper.updatePassword(setPassword, user.getEmail());
+					
+		
+			return newPassword;
+		}
+		
 		
 }
