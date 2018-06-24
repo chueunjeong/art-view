5cacfc678eade8a5ad9b0369a4a4d2f46894131e
irissun9602 <iris9602@naver.com>
2018-06-24 19:32:07 +0900

    인증 메일 발송/ 링크 클릭 시 계정 할성화/ 임시 비밀번호 발급 컨트롤러 추가

--- src/main/java/skhu/artview/controller/UserController.java
+++ src/main/java/skhu/artview/controller/UserController.java
@@ -1,13 +1,21 @@
 package skhu.artview.controller;
 
+import javax.mail.MessagingException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import skhu.artview.model.EmailAndName;
+import skhu.artview.model.TestEmail;
+import skhu.artview.service.EmailService;
+import skhu.artview.service.UserService;
 import skhu.artview.dto.User;
 import skhu.artview.mapper.UserMapper;
 
@@ -44,4 +52,50 @@ UserMapper userMapper;
         userMapper.delete(id);
         return "redirect:list";
     }
+    
+    
+    
+    @Autowired
+	EmailService emailService;
+	@Autowired
+	UserService userService;
+
+	@RequestMapping(value = "sendMail/{email}", method = RequestMethod.POST)
+	public void send(@PathVariable("email") String email) throws MessagingException {
+		
+		
+		TestEmail testEmail = new TestEmail("iris3795@gmail.com", email, "제목입니다", "http://localhost:8080/RestServerBasic/api/enabled/"+email);
+		emailService.sendMail(testEmail);
+	}
+	
+	@RequestMapping(value = "enabled/{email}", method = RequestMethod.GET)
+	public String enabled(@PathVariable("email") String email) throws MessagingException {
+		
+		User user = userService.unabledUserByEmail(email);
+		userService.setEnabled(user);
+		
+		
+		return "계정정보가 활성화되었습니다.";
+	}
+	
+	
+
+	@RequestMapping(value = "newPassword", produces = "application/json; charset=utf8",
+						method = RequestMethod.POST)
+	public String newPassword(@RequestBody EmailAndName en) throws MessagingException {
+
+		User user  = userService.findUserByEmailAndName(en);
+
+		if (user != null) {
+			String sendPassword = userService.setNewPassword(user);
+
+			TestEmail testEmail = new TestEmail("iris3795@gmail.com", en.getEmail(), "제목입니다", sendPassword);
+			emailService.sendMail(testEmail);
+
+			return "발송완료";
+		}
+		
+		return "회원정보가 올바르지 않아 발송에 실패했습니다.";
+	}
+
 }
