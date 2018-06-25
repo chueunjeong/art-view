71b571badacac680f68f8e5f63d8baa1249f1bf5
irissun9602 <iris9602@naver.com>
2018-06-25 22:40:54 +0900

    feat: Save answer

--- src/main/java/skhu/artview/service/AnswerService.java
+++ src/main/java/skhu/artview/service/AnswerService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import skhu.artview.dto.Answer;
+import skhu.artview.dto.Question;
 import skhu.artview.mapper.AnswerMapper;
 import skhu.artview.mapper.UserMapper;
 import skhu.artview.model.QnADetail;
@@ -28,6 +29,19 @@ public class AnswerService {
 		return qnaDetail;
 	}
 		
-	
-	
+	//id로 답변 한 개만
+	public Answer getAnswerById(int id) {
+			
+			return answerMapper.findOne(id);
+
+		}
+	//답변 등록
+	public String SaveA(Answer a) {
+		answerMapper.insertA(a);
+		
+		if(getAnswerById(a.getId()) != null) {
+			return "저장 완료";
+		}
+		return "저장 실패";
+	}
 }
