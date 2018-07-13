dd412780192c9f9b0c7cfc456141f53e70c5fdff
hayoung <wizardlover_@naver.com>
2018-07-13 21:24:11 +0900

    fix: UserMapper.java

--- src/main/java/skhu/artview/mapper/UserMapper.java
+++ src/main/java/skhu/artview/mapper/UserMapper.java
@@ -32,8 +32,8 @@ public interface UserMapper {
 	void saveNormal (User user); //일반회원 테스트용
 	void delete (int id);
 
-	List<User> findMyApplicant(int projectId); //프로젝트 신청자 리스트
-	List<User> findMyMember(int projectId); //프로젝트 멤버 리스트
+	List<User> findMyApplicant(int project_id); //프로젝트 신청자 리스트
+	List<User> findMyMember(int project_id); //프로젝트 멤버 리스트
 
 
 	User findByEmail(String email);
